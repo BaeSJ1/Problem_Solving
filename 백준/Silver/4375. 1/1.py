0dfa780eadb9bbf4adf71d7bfcc5c4678b09bd86
BaeSJ1 <155939583+BaeSJ1@users.noreply.github.com>
2024-10-02 16:19:40 +0900

    [Silver III] Title: 1, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/4375. 1/1.py
@@ -0,0 +1,23 @@
+# python
+
+'''
+    1 % 7 = 1
+    11 % 7 = (1 * 10 +1) % 7
+    11111 % 7 = (1111 * 10 + 1) % 7 = ((1111 % 7) * 10 + 1) % 7
+'''
+while True:
+    try:
+        n = int(input())
+    except:
+        break
+
+    num = 0
+    i = 1
+    while True:
+        num = num * 10 + 1  # num은 1부터 11,111,1111 계속 커지는 역할
+        num %= n
+        if num == 0:
+            print(i)
+            break
+        else:
+            i += 1
