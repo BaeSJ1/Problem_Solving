c371509d5d8d7039b8fdb75f3aa6d1efc1ad3e24
BaeSJ1 <155939583+BaeSJ1@users.noreply.github.com>
2024-10-03 18:46:27 +0900

    [Gold IV] Title: 약수의 합, Time: 224 ms, Memory: 133916 KB -BaekjoonHub

--- 백준/Gold/17425. 약수의 합/약수의 합.py
+++ 백준/Gold/17425. 약수의 합/약수의 합.py
@@ -1,16 +1,29 @@
-MAX = 1000000;
-d = [1]*(MAX+1)
-s = [0]*(MAX+1)
-for i in range(2, MAX+1):
+# python
+'''
+   # g(N) = f(1) + f(2) + ... + f(N)
+   # f[i] 1로 세팅, i=2부터 시작, i*j<=n까지,
+'''
+
+MAX = 1000000
+f = [1] * (MAX + 1)
+g = [0] * (MAX + 1)
+
+for i in range(2, MAX + 1):
     j = 1
-    while i*j <= MAX:
-        d[i*j] += i
+    while i * j <= MAX:
+        f[i * j] += i
         j += 1
-for i in range(1, MAX+1):
-    s[i] = s[i-1] + d[i]
+
+for j in range(1, MAX + 1):
+    g[j] = g[j - 1] + f[j]
+
 t = int(input())
-ans = []
-for _ in range(t):
+sum_val = []
+
+for _ in range(1, t + 1):
     n = int(input())
-    ans.append(s[n])
-print('\n'.join(map(str,ans))+'\n')
+    sum_val.append(g[n])
+
+print('\n'.join(map(str, sum_val)) + '\n')
+
+print()
