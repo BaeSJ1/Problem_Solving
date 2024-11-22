ebf2128f78f8041a34923985abd06fad4e1933d9
BaeSJ1 <155939583+BaeSJ1@users.noreply.github.com>
2024-11-22 21:12:33 +0900

    [Silver III] Title: 소수 구하기, Time: 720 ms, Memory: 44620 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -5,24 +5,34 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        long[] prime = new long[MAX];
-        boolean[] check = new boolean[MAX];
+        long[] primes = new long[MAX + 1]; // 0 ~ MAX 까지
+        boolean[] check = new boolean[MAX + 1];
 
-        int pn = 0;
         int m = sc.nextInt();
         int n = sc.nextInt();
 
-        for (int i = 2; i <= n; i++) {
+        // 배열 초기화
+        for (int i = 2; i < MAX; i++) {
+            check[i] = false; // 아직 방문 안함
+        }
+
+        check[0] = check[1] = true;
+
+        int pn = 0;
+        for (int i = 2; i * i <= MAX; i++) {
             if (!check[i]) {
-                prime[pn++] = i;
-                for (int j = i * 2; j <= n; j += i) {  //i의 배수를 지운다.
+                primes[pn++] = i;
+                for (int j = i * i; j <= MAX; j += i) {
                     check[j] = true;
                 }
             }
         }
-        for (int i = 0; i < pn; i++) {
-            if(prime[i] < m) continue;
-            System.out.println(prime[i]);
+
+        // m 이상 n 이하의 소수 출력
+        for (int i = m; i <= n; i++) {
+            if (!check[i] && i >= 2) {  // 소수이면서 2 이상일 때 출력
+                System.out.println(i);
+            }
         }
     }
 }
