7b799f6e980f425e777a56f77c8d864bb4927b49
BaeSJ1 <155939583+BaeSJ1@users.noreply.github.com>
2024-11-23 22:00:09 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 1628 ms, Memory: 129268 KB -BaekjoonHub

--- 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.java
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.java
@@ -1,63 +1,39 @@
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     static final int MAX = 1000000;
-    static boolean[] isPrime = new boolean[MAX + 1]; // 소수 여부 저장
-    static ArrayList<Integer> primes = new ArrayList<>(); // 소수 리스트
 
     public static void main(String[] args) {
+        boolean[] check = new boolean[MAX + 1]; // 0 ~ MAX 까지
+        ArrayList<Integer> primes = new ArrayList<Integer>();
         Scanner sc = new Scanner(System.in);
 
-        // 1. 에라토스테네스의 체로 소수 생성
-        generatePrimes();
+        // 에라토스테네스의 체
+        check[0] = check[1] = true;
 
-        // 2. 입력 처리
-        while (true) {
-            int n = sc.nextInt();
-            if (n == 0) { // 0 입력 시 종료
-                break;
-            }
-            // 3. 골드바흐의 추측 결과 출력
-            if (!goldbachConjecture(n)) {
-                System.out.println("Goldbach's conjecture is wrong.");
-            }
-        }
-    }
-
-    // 에라토스테네스의 체
-    static void generatePrimes() {
-        for (int i = 2; i <= MAX; i++) {
-            isPrime[i] = true; // 초기값 설정
-        }
-
+        // primes 배열에 소수 값들 저장
         for (int i = 2; i * i <= MAX; i++) {
-            if (isPrime[i]) {
+            if (!check[i]) {
+                primes.add(i);
                 for (int j = i * i; j <= MAX; j += i) {
-                    isPrime[j] = false; // 배수 제거
+                    check[j] = true;
                 }
             }
         }
 
-        // 소수 리스트 저장
-        for (int i = 2; i <= MAX; i++) {
-            if (isPrime[i]) {
-                primes.add(i);
+        while (true) {
+            int n = sc.nextInt();
+            if (n == 0) break;
+            for (int i=1; i<primes.size(); i++){
+                int a = primes.get(i);
+                if(check[n - a] == false && a % 2 != 0){
+                    System.out.println(n + " = " + a + " + " + (n - a));
+                    break;
+                }
             }
-        }
-    }
 
-    // 골드바흐의 추측 검증
-    static boolean goldbachConjecture(int n) {
-        for (int prime : primes) {
-            if (prime >= n) break; // prime이 n 이상이면 탐색 종료
-            int b = n - prime; // 두 번째 소수 계산
-            if (isPrime[b]) { // b가 소수인지 확인
-                System.out.println(n + " = " + prime + " + " + b);
-                return true;
-            }
         }
-        return false; // 실패 시 false 반환
+
     }
 }
