fc85dc3ca8ecac27dec300d5259f9e617baa30ff
BaeSJ1 <155939583+BaeSJ1@users.noreply.github.com>
2024-12-14 19:33:25 +0900

    [Bronze I] Title: 진법 변환 2, Time: 176 ms, Memory: 17788 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -3,34 +3,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int b = sc.nextInt();
 
-        // 10진수 숫자와 변환할 진법 입력
-        int decimal = sc.nextInt();
-        int base = sc.nextInt();
-
-        // 결과 출력
-        System.out.println(convertToBase(decimal, base));
-    }
-
-    // 10진수를 N진법으로 변환하는 함수
-    public static String convertToBase(int decimal, int base) {
         StringBuilder result = new StringBuilder();
 
-        // 0일 경우 바로 반환
-        if (decimal == 0) {
-            return "0";
+        while (n > 0) {
+            int value = n % b;
+            if (value < 10) {
+                result.append((char) (value + '0'));
+            } else {
+                result.append((char) (value - 10 + 'A'));
+            }
+            n /= b;
         }
-
-        // 10 이상의 숫자는 알파벳으로 표현
-        char[] digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
-
-        // N진법으로 변환
-        while (decimal > 0) {
-            result.append(digits[decimal % base]); // 나머지를 결과에 추가
-            decimal /= base;                       // 몫을 업데이트
-        }
-
-        // 거꾸로 계산했으므로 뒤집기
-        return result.reverse().toString();
+        System.out.println(result.reverse().toString());
     }
 }
