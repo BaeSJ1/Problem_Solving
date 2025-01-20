33ac8090591739f5eb1b3d4802b055c92e494402
BaeSJ1 <155939583+BaeSJ1@users.noreply.github.com>
2025-01-20 21:17:32 +0900

    [level 2] Title: 올바른 괄호, Time: 17.57 ms, Memory: 54.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,21 @@
+import java.util.ArrayDeque;
+
+class Solution {
+    boolean solution(String s) {
+        ArrayDeque<Character> stack = new ArrayDeque<>();
+        
+        char[] a = s.toCharArray();
+        for(char c:a){
+            if(c == '('){
+                stack.push(c);
+            } else {
+                if(stack.isEmpty()) {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
