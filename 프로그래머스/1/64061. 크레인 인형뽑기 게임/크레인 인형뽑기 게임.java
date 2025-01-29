55c3453adc01b338c23a8af3aa52f060250735d0
BaeSJ1 <155939583+BaeSJ1@users.noreply.github.com>
2025-01-29 19:14:42 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.29 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -2,34 +2,37 @@ import java.util.ArrayDeque;
 class Solution {
     public int solution(int[][] board, int[] moves) {
         ArrayDeque<Integer>[] stack = new ArrayDeque[board.length];
-
-        for (int i = 0; i < stack.length; i++) {
+        int result = 0;
+        
+        for(int i = 0; i < board.length; i++){
             stack[i] = new ArrayDeque<>();
         }
-
-        // 2차원 배열 값 스택에 할당
-        for (int i = board.length - 1; i >= 0; i--) {
-            for (int j = 0; j < board[i].length; j++) {
+        
+        for(int i = board.length - 1; i >= 0; i--){
+            for(int j = 0; j < board[i].length; j++){
                 if(board[i][j] > 0){
-                    stack[j].push(board[i][j]);
+                    stack[j].push(board[i][j]);  // 0 ~ 4번 레일에 값 할당
                 }
             }
         }
-
-        ArrayDeque<Integer> resultStack = new ArrayDeque<>();
-        int result = 0;
-
+        
+        
+        ArrayDeque<Integer> resultStack = new ArrayDeque<Integer>();
+        
         for(int move: moves){
             if(!stack[move - 1].isEmpty()){
-                int doll = stack[move-1].pop();
-                if(!resultStack.isEmpty() && resultStack.peek() == doll){
-                    resultStack.pop();
+                int doll = stack[move - 1].pop();
+                
+                // 결과 스택에 동일한 값이 top에 존재한다면
+                if(!resultStack.isEmpty() && doll == resultStack.peek()){
+                   resultStack.pop();
                     result += 2;
                 } else{
                     resultStack.push(doll);
                 }
             }
         }
+        
         return result;
     }
 }
