79526a49a4837f43716e5cd9a16c75e9497d6341
BaeSJ1 <155939583+BaeSJ1@users.noreply.github.com>
2025-02-18 21:08:09 +0900

    [level 2] Title: 방문 길이, Time: 19.83 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -1,34 +1,31 @@
-import java.util.HashMap;
-import java.util.HashSet;
-
+import java.util.*;
 class Solution {
+    private static boolean initValid(int nx, int ny){
+        return 0 <= nx && nx < 11 && 0 <= ny && ny < 11;
+    }
     public int solution(String dirs) {
         HashMap<Character, int[]> location = new HashMap<>();
-        HashSet<String> result = new HashSet<>();
+        location.put('U', new int[]{0,1});
+        location.put('D', new int[]{0,-1});
+        location.put('R', new int[]{1,0});
+        location.put('L', new int[]{-1,0});
         
-        location.put('U', new int[]{0, -1});
-        location.put('D', new int[]{0, 1});
-        location.put('R', new int[]{1, 0});
-        location.put('L', new int[]{-1, 0});
-        
-        int x = 0; 
-        int y = 0;
-        
+        int x = 5, y = 5;
+        HashSet<String> s = new HashSet<>();
         for(int i = 0; i < dirs.length(); i++){
+            // 입력받은 값을 한글자씩 반환
             int[] offset = location.get(dirs.charAt(i));
-            
             int nx = x + offset[0];
             int ny = y + offset[1];
             
-            if (-5 > nx || nx > 5 || -5 > ny || ny > 5) continue;
+            if(!initValid(nx, ny)) continue;
             
-            result.add(nx + " " + ny + " " + x + " " + y);
-            result.add(x + " " + y + " " + nx + " " + ny);
+            s.add(x + " " + y + " " + nx + " " + ny);
+            s.add(nx + " " + ny + " " + x + " " + y);
             
             x = nx;
             y = ny;
         }
-        
-        return result.size() / 2;
+        return s.size()/2;
     }
 }
