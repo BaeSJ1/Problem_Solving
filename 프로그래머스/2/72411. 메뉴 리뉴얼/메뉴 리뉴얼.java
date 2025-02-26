077f7b0382cee49614c9d2e48147c9b158a866c9
BaeSJ1 <155939583+BaeSJ1@users.noreply.github.com>
2025-02-26 20:07:37 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 26.56 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -1,40 +1,42 @@
 import java.util.*;
+
 class Solution {
+    // {코스 구성 개수 : {코스 구성 -> 주문이 들어온 횟수}}
     private static HashMap<Integer, HashMap<String, Integer>> courseMap;
     
     public String[] solution(String[] orders, int[] course) {
         courseMap = new HashMap<>();
-        for(int c : course){
+        for(int c: course){
             courseMap.put(c, new HashMap<>());
         }
         
         for(String order: orders){
+            // 배열로 넘겨야지 한글자씩 검사가능
             char[] orderArray = order.toCharArray();
+            // AB = BA 같게 처리하기 위해서
             Arrays.sort(orderArray);
             combinations(0, orderArray, "");
         }
         
-        ArrayList<String> answer = new ArrayList<>();
+        ArrayList<String> result = new ArrayList<>();
         
-        for (HashMap<String, Integer> count : courseMap.values()) {
-            count.values()
-                    .stream()
-                    .max(Comparator.comparingInt(o -> o)) // ❸ 가장 빈도수가 높은 코스를 찾음
-                    .ifPresent(cnt -> count.entrySet() // ❹ 코스에 대한 메뉴 수가 가능할 때만
-                            .stream()
-                            // ❺ 최소 2명 이상의 손님으로부터 주문된 단품메뉴 조합에 대해서만
-                            .filter(entry -> cnt.equals(entry.getValue()) && cnt > 1)
-                            // ❻ 코스 메뉴만 answer 리스트에 추가
-                            .forEach(entry -> answer.add(entry.getKey())));
+        for(HashMap<String, Integer> count: courseMap.values()){
+            count.values().stream()
+                .max(Comparator.comparingInt(o -> o))
+                .ifPresent(cnt -> count.entrySet().stream()
+                          .filter(entry -> cnt.equals(entry.getValue()) && cnt > 1)
+                          .forEach(entry -> result.add(entry.getKey())));
         }
-        Collections.sort(answer);
-        return answer.toArray(new String[0]);
+        Collections.sort(result);
+        return result.toArray(new String[0]);
     }
     
-    public static void combinations(int idx, char[] order, String result){
+    private static void combinations(int idx, char[] order, String result){
+        // courseMap의 Key는 구성하려는 코스 개수이다.
+        // 구성하려는 코드 개수와 같다면 주문 몇번 들어온지 센다.
         if(courseMap.containsKey(result.length())){
             HashMap<String, Integer> map = courseMap.get(result.length());
-            map.put(result, map.getOrDefault(result, 0) + 1);
+            map.put(result, map.getOrDefault(result, 0) +1);
         }
         
         for(int i = idx; i < order.length; i++){
