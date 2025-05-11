6c07038542636f4047a17f043a14cc1666b51999
BaeSJ1 <155939583+BaeSJ1@users.noreply.github.com>
2025-05-11 14:22:37 +0900

    [level 2] Title: 주식가격, Time: 32.85 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,24 +1,30 @@
-import java.util.ArrayDeque;
+import java.util.*;
+
+/*
+초 누적 -> stack
+stack에 인덱스의 값을 넣고 인덱스 - 인덱스와 같은 형태로 초를 계산
+만약 가격이 떨어지는 부분이 나오면, 인덱스 - 인덱스를 계산해서 반환
+*/
 
 class Solution {
     public int[] solution(int[] prices) {
+        int n = prices.length;
+        int[] result = new int[n];
+        
         ArrayDeque<Integer> stack = new ArrayDeque<>();
         
-        int[] result = new int[prices.length];
-        
-        stack.push(0);
-        for(int current = 1; current < prices.length; current++){
-            while(!stack.isEmpty() && prices[stack.peek()] > prices[current]){
-                int index_value = stack.pop();
-                result[index_value] = current - index_value;
+        for(int i = 0; i < n; i++){
+            // 값이 떨어지면
+            while(!stack.isEmpty() && prices[stack.peek()] > prices[i]){
+                result[stack.peek()] = i - stack.peek();
+                stack.pop();
             }
-            stack.push(current);
+            stack.push(i);
         }
         
-        // 배열 다 돌았는데 스택에 값이 남아있으면 빼야한다.
         while(!stack.isEmpty()){
-            int index_value = stack.pop();
-            result[index_value] = prices.length - index_value - 1;
+            result[stack.peek()] = n - 1 - stack.peek();
+            stack.pop();
         }
         
         return result;
