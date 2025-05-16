f8c607c2f847f0fe75ef1a44b02bc13d8d5bd681
BaeSJ1 <155939583+BaeSJ1@users.noreply.github.com>
2025-05-16 22:36:11 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 77.05 ms, Memory: 121 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -2,23 +2,26 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
-        // 판매자, 추천자 
-        HashMap<String, String> map = new HashMap<>(); 
+        HashMap<String, String> map = new HashMap<>();
+        
+        // 판매자, 추천자
         for(int i = 0; i < enroll.length; i++){
             map.put(enroll[i], referral[i]);
         }
         
-        // 수익 계산
-        // 판매자, 수익
+        // 판매자, 이익
         HashMap<String, Integer> total = new HashMap<>();
         for(int i = 0; i < seller.length; i++){
             int money = amount[i] * 100;
-            String curName = seller[i];
-            while(money > 0 && !curName.equals("-")){
-                total.put(curName, total.getOrDefault(curName, 0) + money - (money / 10));
-                curName = map.get(curName);
-                // 추천자는 10%의 이익을 가져간다.
-                money /= 10; 
+            
+            // 이익에 대한 사람 추적
+            String person = seller[i];
+                
+            while(money >= 1 && !person.equals("-")){
+                total.put(person, total.getOrDefault(person, 0) + money - money / 10);
+                money /= 10;
+                // 다음 추천인으로 이동
+                person = map.get(person);
             }
         }
         
