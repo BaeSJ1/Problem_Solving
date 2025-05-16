6b0ea41ef07684c315d49f1befb828dbd45c6f80
BaeSJ1 <155939583+BaeSJ1@users.noreply.github.com>
2025-05-16 23:19:27 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1104 ms, Memory: 74912 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static List<List<Integer>> graph = new ArrayList<>();
+    private static int[] parent;
+    private static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        parent = new int[n + 1];
+        visited = new boolean[n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            graph.get(a).add(b);
+            graph.get(b).add(a);
+        }
+
+        dfs(1);
+
+        for (int i = 2; i <= n; i++) {
+            System.out.println(parent[i]);
+        }
+    }
+
+    private static void dfs(int node) {
+        visited[node] = true;
+
+        for (int next : graph.get(node)) {
+            if (!visited[next]) {
+                parent[next] = node;
+                dfs(next);
+            }
+        }
+    }
+}
