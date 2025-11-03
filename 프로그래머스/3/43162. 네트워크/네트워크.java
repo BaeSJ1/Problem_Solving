62c45445471f8105fd2c458d80433f889b1a9dc8
BaeSJ1 <155939583+BaeSJ1@users.noreply.github.com>
2025-11-03 19:49:22 +0900

    [level 3] Title: 네트워크, Time: 0.20 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,28 +1,25 @@
+import java.util.*;
 class Solution {
-    private static boolean[] visited;
-    private static int[][] computer;
-    
-    private static void dfs(int now){
-        visited[now] = true;
-        for(int i = 0; i < computer[now].length; i++){
-            if(computer[now][i] == 1 && !visited[i]){
-                dfs(i);
-            }
-        }
-    }
-    
     public int solution(int n, int[][] computers) {
-        computer = computers;
+        // 이미 확인한 네트워크인지 알아야함
+        boolean[] visited = new boolean[n];
         int result = 0;
-        visited = new boolean[n];
         
         for(int i = 0; i < n; i++){
             if(!visited[i]){
-                dfs(i);
+                dfs(computers, visited, i);
                 result++;
             }
         }
-        
         return result;
+    }
+    
+    static void dfs(int[][] computers, boolean[] visited, int index){
+        visited[index] = true;
+        for(int i = 0; i < computers[index].length; i++){
+            if(computers[index][i] == 1 && !visited[i]){
+                dfs(computers, visited, i);
+            }
+        }
     }
 }
