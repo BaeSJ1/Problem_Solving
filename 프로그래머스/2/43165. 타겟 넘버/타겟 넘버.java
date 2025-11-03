adc0f14d69b7d2878b31603b5dba15400b46e04b
BaeSJ1 <155939583+BaeSJ1@users.noreply.github.com>
2025-11-03 18:20:14 +0900

    [level 2] Title: 타겟 넘버, Time: 6.51 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,26 +1,22 @@
 import java.util.*;
-
-// DFS 사용
-
 class Solution {
-    private static int count = 0;
+    private static int result;
     public int solution(int[] numbers, int target) {
-        // target과 같아야한다.
+        result = 0;
         
-        // 입력배열, 인덱스, 현재까지 합, 목표 값
         dfs(numbers, 0, 0, target);
-        return count;
+        return result;
     }
     
-    private static void dfs(int[] numbers, int idx, int sum, int target){
-        if(idx == numbers.length){
-            if(sum == target){
-                count++;
+    private static void dfs(int[] numbers, int index, int sum, int target){
+        if(numbers.length == index){
+            if(target == sum){
+                result++;
             }
             return;
         }
         
-        dfs(numbers, idx + 1, sum + numbers[idx], target);
-        dfs(numbers, idx + 1, sum - numbers[idx], target);
+        dfs(numbers, index + 1, sum + numbers[index], target);
+        dfs(numbers, index + 1, sum - numbers[index], target);
     }
 }
