2c00bb1df48c0d2f7271fa9876df02b7b327b27c
BaeSJ1 <155939583+BaeSJ1@users.noreply.github.com>
2025-11-04 15:00:59 +0900

    [level 2] Title: 타겟 넘버, Time: 62.31 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,22 +1,44 @@
+// 14:42 - 
+// 스택 dfs
+
 import java.util.*;
 class Solution {
-    private static int result;
+    static class State{
+        int depth;
+        int sum;
+        State(int depth, int sum){
+            this.depth = depth;
+            this.sum = sum;
+        }
+    }
+    
+    static private int result = 0;
+    
     public int solution(int[] numbers, int target) {
-        result = 0;
-        
-        dfs(numbers, 0, 0, target);
+        dfs(numbers, target);
         return result;
     }
     
-    private static void dfs(int[] numbers, int index, int sum, int target){
-        if(numbers.length == index){
-            if(target == sum){
-                result++;
+    static void dfs(int[] numbers, int target){
+        Deque<State> stack = new ArrayDeque<>();
+        stack.add(new State(0, 0));
+        
+        while(!stack.isEmpty()){
+            State now = stack.pop();
+            
+            // 제일 하위 트리까지 계산했으면, target과 같은 지 비교
+            if(now.depth == numbers.length){
+                if(target == now.sum){
+                    result++;
+                }
+                continue;
             }
-            return;
+            
+            
+            // 다음 스택엔 현재까지 계산한 sum이랑 다음에 탐색할 depth 정보를 줘야함
+            stack.push(new State(now.depth + 1, now.sum + numbers[now.depth]));
+            stack.push(new State(now.depth + 1, now.sum - numbers[now.depth]));
         }
         
-        dfs(numbers, index + 1, sum + numbers[index], target);
-        dfs(numbers, index + 1, sum - numbers[index], target);
     }
 }
