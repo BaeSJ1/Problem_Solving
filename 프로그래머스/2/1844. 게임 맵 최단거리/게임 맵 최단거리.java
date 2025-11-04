14a0c94b5e65138d6ec3320ae38fcf45c13bfdda
BaeSJ1 <155939583+BaeSJ1@users.noreply.github.com>
2025-11-04 16:11:50 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.87 ms, Memory: 54.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,53 +1,63 @@
+// 최단 거리 -> bfs
+// 15:38 - 
+
 import java.util.*;
+
 class Solution {
-    // 방향 (위, 아래, 오른쪽, 왼쪽)
-    private static final int[] nx = {-1, 1, 0, 0};  // 행으로 생각
-    private static final int[] ny = {0, 0, 1, -1};  // 열로 생각
-    private static class Node{
-        int row;
-        int col;
-        
-        public Node(int row, int col){
-            this.row = row;
-            this.col = col;
+    // 위, 아래, 왼, 오
+    static final int[] dx = {-1, 1, 0, 0};
+    static final int[] dy = {0, 0, -1, 1};
+    
+    static class Node{
+        final int x;
+        final int y;
+        Node(int x, int y){
+            this.x = x;
+            this.y = y;
         }
     }
+    
     public int solution(int[][] maps) {
+        
+        return bfs(maps);
+        
+    }
+    
+    static int bfs(int[][] maps){
         int n = maps.length;
         int m = maps[0].length;
         
-        // 최소 거리를 저장하는 dist배열
+        // 최소 거리를 담는다.
         int[][] dist = new int[n][m];
         
-        // bfs를 위한 큐 생성
-        ArrayDeque<Node> queue = new ArrayDeque<>();
-        
+        Deque<Node> queue = new ArrayDeque<>();
         queue.add(new Node(0, 0));
-        dist[0][0] = 1;
         
         while(!queue.isEmpty()){
             Node now = queue.poll();
             
-            // 현재 위치에서 이동할 수 있는 모든 방향
             for(int i = 0; i < 4; i++){
-                int x = now.row + nx[i];
-                int y = now.col + ny[i];
+                // 이동위치 계산
+                int nx = now.x + dx[i];
+                int ny = now.y + dy[i];
                 
-                /// 범위 내에 있고 , 이미 방문한 노드가 아니고, maps의 값이 0이 아니면 된다.
-                // 범위 검사
-                if(x < 0 || y <0 || x >= n|| y >= m) continue;
+                // 계산된 위치가 범위를 벗어나는지 체크
+                if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
                 
-                // 벽으로 가는 경우 예외 처리
-                if(maps[x][y] == 0) continue;
+                // 벽인 것도 패스
+                if(maps[nx][ny] == 0) continue;
                 
+                // dist 값이 없는(0) 곳은 계산이 필요하다.
+                if(dist[nx][ny] == 0){
+                    queue.add(new Node(nx, ny));
+                    dist[nx][ny] = dist[now.x][now.y] + 1;
+                }
                 
-                // 처음 방문한 경우, queue에 추가하고 거리를 갱신
-                if(dist[x][y] == 0){
-                    queue.add(new Node(x, y));
-                    dist[x][y] = dist[now.row][now.col] + 1;
+                if((nx == (n - 1) && ny == (m - 1)) && dist[nx][ny] != 0 ){
+                    return dist[nx][ny] + 1;
                 }
             }
         }
-        return dist[n - 1][m - 1] == 0 ? -1 : dist[n-1][m-1];
+        return -1;
     }
 }
