cc61d1c68d3e64bdede42ce164dc947992b2ff55
BaeSJ1 <155939583+BaeSJ1@users.noreply.github.com>
2025-11-07 22:10:02 +0900

    [level 1] Title: 모의고사, Time: 4.42 ms, Memory: 87.1 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,29 +1,39 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+// 21:45-22:09
+import java.util.*;
+
 class Solution {
+    static int[] p1 = {1, 2, 3, 4, 5};
+    static int[] p2 = {2, 1, 2, 3, 2, 4, 2, 5};
+    static int[] p3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
     public int[] solution(int[] answers) {
-        int[][] pattern = {
-            {1, 2, 3, 4, 5},
-            {2, 1, 2, 3, 2, 4, 2, 5},
-            {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
-        };
-        int[] score = new int[3];
-        for(int i = 0; i < pattern.length; i++){
-            for(int j = 0; j < answers.length; j++){
-                if(answers[j] == pattern[i][j % pattern[i].length]){
-                    score[i] += 1;
-                }
+        int[] count = new int[3];
+        for(int i = 0; i < answers.length; i++){
+            if(answers[i] == p1[i % p1.length]){
+                count[0]++;
+            }
+            if(answers[i] == p2[i % p2.length]){
+                count[1]++;
+            }
+            if(answers[i] == p3[i % p3.length]){
+                count[2]++;
             }
         }
-        int maxScore = Arrays.stream(score).max().getAsInt();
-        ArrayList<Integer> result = new ArrayList<>();
-        for(int s = 0; s < score.length; s++){
-            if(score[s] == maxScore){
-                result.add(s + 1);
+        
+        int[] copy = new int[3];
+        
+        copy = count.clone();
+        Arrays.sort(copy);
+        
+        int max = copy[copy.length - 1];
+        
+        List<Integer> result = new ArrayList<>();
+        
+        for(int i = 0; i < 3; i++){
+            if(count[i] == max){
+                result.add(i + 1);
             }
         }
-        return result.stream().mapToInt(Integer::intValue).toArray();
         
-        
+        return result.stream().mapToInt(i->i).toArray();
     }
 }
