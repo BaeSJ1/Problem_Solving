92a45089217c6b231f83d2b25ef115742982e1ef
BaeSJ1 <155939583+BaeSJ1@users.noreply.github.com>
2025-11-11 12:57:06 +0900

    [level 1] Title: 모의고사, Time: 9.36 ms, Memory: 87.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,39 +1,40 @@
-// 21:45-22:09
+//12:43 -  
 import java.util.*;
 
 class Solution {
     static int[] p1 = {1, 2, 3, 4, 5};
     static int[] p2 = {2, 1, 2, 3, 2, 4, 2, 5};
     static int[] p3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+    
     public int[] solution(int[] answers) {
-        int[] count = new int[3];
+        int[] result = new int[3];
+        List<Integer> answer = new ArrayList<>();
+        
         for(int i = 0; i < answers.length; i++){
             if(answers[i] == p1[i % p1.length]){
-                count[0]++;
+                result[0]++;
             }
-            if(answers[i] == p2[i % p2.length]){
-                count[1]++;
+            
+            if(answers[i] == p2[i%p2.length]){
+                result[1]++;
             }
-            if(answers[i] == p3[i % p3.length]){
-                count[2]++;
+            
+            if(answers[i] == p3[i%p3.length]){
+                result[2]++;
             }
         }
         
-        int[] copy = new int[3];
+        int[] arr = result.clone();
         
-        copy = count.clone();
-        Arrays.sort(copy);
-        
-        int max = copy[copy.length - 1];
-        
-        List<Integer> result = new ArrayList<>();
+        Arrays.sort(arr);
+        int max = arr[2];
         
         for(int i = 0; i < 3; i++){
-            if(count[i] == max){
-                result.add(i + 1);
+            if(max == result[i]){
+                answer.add(i+1);
             }
         }
         
-        return result.stream().mapToInt(i->i).toArray();
+        return answer.stream().mapToInt(i->i).toArray();
     }
 }
