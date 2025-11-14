537cc08b2a3c7e0a7d69b31e1f23b1d7acb4ff28
BaeSJ1 <155939583+BaeSJ1@users.noreply.github.com>
2025-11-14 17:36:21 +0900

    [level 3] Title: 등굣길, Time: 1.10 ms, Memory: 54.4 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,38 +1,39 @@
 import java.util.*;
 
 class Solution {
-    static int MOD =  1_000_000_007;
+    static final int MOD = 1_000_000_007;
+    
+    // 상, 하, 좌, 우
+    static final int[] dx = {-1, 1, 0, 0};
+    static final int[] dy = {0, 0, -1, 1};
+    
     public int solution(int m, int n, int[][] puddles) {
-        
         int result = calculateDp(m, n, puddles);
         return result;
     }
     
-    private int calculateDp(int m, int n, int[][] puddles){
-        // (1,1)로 시작한다.
+    static int calculateDp(int m, int n, int[][] puddles){
+        // dp에 이동 할 수 있는 가지수 누적함.
         int[][] dp = new int[n + 1][m + 1];
         
-        // 물 웅덩이
+        dp[1][1] = 1;
+        
         boolean[][] water = new boolean[n + 1][m + 1];
         for(int[] p: puddles){
             water[p[1]][p[0]] = true;
         }
         
-        dp[1][1] = 1;
         for(int i = 1; i <= n; i++){
-            for(int j = 1; j <=m; j++){
-                if(water[i][j]){
-                    dp[i][j] = 0;
-                    continue;
-                }
-                
+            for(int j = 1; j <= m; j++){
                 if(i == 1 && j == 1) continue;
-                
-                // 왼쪽에서 오는거랑 오른쪽에서 오는 경우 더함
-                dp[i][j] = ((dp[i - 1][j] + dp[i][j - 1]) % MOD);
+                if(water[i][j]) dp[i][j] = 0;
+                else{
+                    // 왼쪽과 위쪽에서 오는 경우의 수 더하기
+                    // 여기에 MOD 나머지 연산안하면 효율성에서 0점 나옴
+                    dp[i][j] = (dp[i][j - 1] + dp[i - 1][j]) % MOD;
+                }
             }
         }
-        
         return dp[n][m];
     }
 }
