87a313556ab8884d9dc3305e1542d4a10867bdac
BaeSJ1 <155939583+BaeSJ1@users.noreply.github.com>
2025-11-14 17:09:50 +0900

    [level 3] Title: 정수 삼각형, Time: 12.91 ms, Memory: 63.5 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,38 +1,37 @@
 import java.util.*;
 
+// dp의 bottom-up 방식 사용
+// [i][0] 이나 [i][m - 1] 부분은 비교 없어도 됨
+
 class Solution {
     public int solution(int[][] triangle) {
         int result = calculateDp(triangle);
         return result;
     }
     
-    private int calculateDp(int[][] triangle){
-        int n = triangle.length;
-        int[][] dp = new int[n][n];
+    static int calculateDp(int[][] tri){
+        int n = tri.length;
+        int m = tri[n - 1].length;
         
-        // 시작점
-        dp[0][0] = triangle[0][0];
+        int[][] dp = new int[n][m];
+        dp[0][0] = tri[0][0];
         
-        // dp[0] 지정했으니까 i = 1부터 시작해야함
-        // 삼각형의 특성상 행번호 + 1 = 열임
-        // 1번째 행 -> 2개 들어감
         for(int i = 1; i < n; i++){
             for(int j = 0; j <= i; j++){
-                // 한 행의 맨 앞이나 맨 뒤는 그냥 계산해서 바로 넣으면 됨
                 if(j == 0){
-                    dp[i][j] = dp[i - 1][0] + triangle[i][0];
+                    dp[i][0] = dp[i - 1][0] + tri[i][0];
                 } else if(j == i){
-                    dp[i][j] = dp[i - 1][j - 1] + triangle[i][j];
+                    dp[i][j] = dp[i - 1][j - 1] + tri[i][j];
                 } else{
-                    dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i - 1][j]) + triangle[i][j];
+                    dp[i][j] = Math.max(dp[i - 1][j - 1] + tri[i][j], dp[i - 1][j] + tri[i][j]);
                 }
             }
         }
         
         int max = 0;
         
-        // 마지막 줄에서 가장 큰 값이 정답
-        for(int j = 0; j < n; j++){
+        // 마지막 depth에서 가장 큰 값 계산해야함
+        for(int j = 0; j < m; j++){
             max = Math.max(max, dp[n - 1][j]);
         }
         
