ebbcfa1ed38e1bfa255741d4a4613f9cdf5b2d95
BaeSJ1 <155939583+BaeSJ1@users.noreply.github.com>
2026-06-18 16:27:30 +0900

    [level 1] Title: 약수의 합, Time: 0.03 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int n) {
+        int total = 0;
+        for(int i = 1; i <= n/2; i++){
+            if(n % i == 0) total += i;
+        }
+        return total + n;
+    }
+}
